bedb176417a92cc33d8b4abc25b8dd1f84b1cf40
Balogun Olamide <olamidebalo214@gmail.com>
2021-11-03 21:23:42 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const cookieParser = require("cookie-parser");
+const authRoutes = require("./routes/authRoutes");
+const { requireAuth, checkUser } = require("./middleware/authmiddleware");
+
+const port = 4000 || process.env.port;
+
+// middleware
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cookieParser());
+
+// view engine
+app.set("view engine", "ejs");
+
+// database connection
+const dbURI = "mongodb+srv://olamide:1234@cluster0.f7hgm.mongodb.net/node-proj";
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(port))
+  .catch((err) => console.log(err));
+
+// routes
+app.get("*", checkUser);
+app.get("/", requireAuth, (req, res) =>
+  res.render("home", { title: "Homepage" })
+);
+app.use(authRoutes);
---
+++ models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const { isEmail } = require("validator");
+
+const facioSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Please enter an email"],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, "Please enter a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter a password"],
+    minlength: [6, "Minimum password lenght is 6 characters"],
+  },
+});
+
+// To hash password
+facioSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// static method to log users in
+facioSchema.statics.login = async function (email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error("incorrect password");
+  }
+  throw Error("incorrect email");
+};
+
+const User = mongoose.model("facio", facioSchema);
+
+module.exports = User;
